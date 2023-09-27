5609bc8ed82fe112970e64a7f8069d6958705e71
Maciej Gierada <maciej@braidwell.io>
2023-09-27 10:53:51 +0200

    feat: add register-autocmd.lua

---
+++ lua/pybumper/utils/register-autocmd.lua
@@ -0,0 +1,8 @@
+local constants = require("package-info.utils.constants")
+
+--- Register given command when the event fires
+-- @param event: string - event that will trigger the autocommand
+-- @param command: string - command to fire when the event is triggered
+return function(event, command)
+    vim.cmd("autocmd " .. constants.AUTOGROUP .. " " .. event .. " pyproject.toml" .. command)
+end
