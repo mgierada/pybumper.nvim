5b35f731235f3728bf866d06d6982172e4001fef
Maciej Gierada <maciej@braidwell.io>
2023-10-01 15:03:23 +0200

    feat: stip >=, >, <=, = from version

--- lua/pybumper/helpers/clean_version.lua
+++ lua/pybumper/helpers/clean_version.lua
@@ -2,9 +2,13 @@
 -- @param value: string - value from which to strip ^ and ~ from
 -- @return string?
 return function(value)
-    if value == nil then
-        return nil
-    end
+	if value == nil then
+		return nil
+	end
 
-    return value:gsub("%^", ""):gsub("~", "")
+	value = value:gsub("%^", ""):gsub("~", "")
+
+	-- Remove >=, >, <, and <= characters
+	value = value:gsub(">=", ""):gsub(">", ""):gsub("<", ""):gsub("<=", "")
+	return value
 end
--- lua/pybumper/parser.lua
+++ lua/pybumper/parser.lua
@@ -77,30 +77,4 @@ M.extract_outdated_dependencies = function(outdated_dependencies)
 	return dependencies
 end
 
--- M.parse_buffer_outdated = function()
--- 	local buffer_lines = vim.api.nvim_buf_get_lines(state.buffer.id, 0, -1, false)
--- 	local buffer_content = table.concat(buffer_lines, "\n")
---
--- 	-- Extract the dependencies section
--- 	-- local dependencies, err = extractDependencies(buffer_content)
--- 	--
--- 	-- local installed_dependencies = {}
--- 	--
--- 	-- if dependencies then
--- 	--     for name, version in pairs(dependencies) do
--- 	--         installed_dependencies[name] = {
--- 	--             current = clean_version(version),
--- 	--         }
--- 	--     end
--- 	-- else
--- 	--     -- Handle the error
--- 	--     logger.error("Error: " .. err)
--- 	-- end
--- 	--
--- 	-- -- Log installed dependencies
--- 	-- logger.warn("Installed dependencies:" .. vim.inspect(installed_dependencies))
--- 	-- state.buffer.lines = buffer_lines
--- 	-- state.dependencies.installed = installed_dependencies
--- end
-
 return M
